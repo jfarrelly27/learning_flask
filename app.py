608d8f1712286d6b8ea95bdd9faa000d39b4e1f9
James M. Farrelly <jimfarrelly27@gmail.com>
2024-04-04 15:05:28 -0500

    Finishing

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world, index'
+    return render_template('index.html')
 
 @app.route('/sum')
 def sum():
@@ -14,4 +14,4 @@ def sum():
     return str(result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
