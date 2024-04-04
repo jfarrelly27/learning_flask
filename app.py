7df71febecba860c4a6f3f2127d1280e41cd4342
James M. Farrelly <jimfarrelly27@gmail.com>
2024-04-04 14:23:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Hello world, index'
+
+@app.route('/sum')
+def sum():
+    n1 = 10
+    n2 = 20
+    result = n1 + n2
+    return str(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
